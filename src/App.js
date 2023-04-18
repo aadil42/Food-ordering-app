3654dc3d971c3830b917d6437e141a24378a18ad
aadil42 <adilkelawala@gmail.com>
2023-04-18 16:38:43 +0530

    added meal summary in layout dir

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // components
 import Header from './Components/Layout/Header';
+import MealSummary from './Components/Layout/MealSummary';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <MealSummary  />
     </React.Fragment>
   );
 }
