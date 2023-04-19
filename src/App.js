f8f66504d21abd8d7abd803e30dcdee18b0ea984
aadil42 <adilkelawala@gmail.com>
2023-04-19 08:50:29 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 // components
 import Header from './Components/Layout/Header';
 import MealSummary from './Components/Layout/MealSummary';
+import MealList from './Components/Meals/MealList';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
       <MealSummary  />
+      <MealList />
     </React.Fragment>
   );
 }
